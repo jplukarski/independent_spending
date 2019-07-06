6e8cbdd7527ec3e633543f6beb2b29659dd7664d
JPlukarski <jwplukarski@gmail.com>
2019-07-05 23:41:40 -0500

    got recharts and semantic custom theme fired up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/home'
 
-function App() {
-  return (
-    <div className="App">
-      Sup.
-    </div>
-  );
-}
+const App = () => (
+
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  </div>
+)
 
 export default App;
